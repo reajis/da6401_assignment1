ce510f62909e0fae9ce4f49ed2379f0190ac3440
reajis <me21b160@smail.iitm.ac.in>
2026-03-07 16:35:48 +0000

    neural network chnag 2

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -1,5 +1,6 @@
 from .neural_layer import NeuralLayer
 
+
 class NeuralNetwork:
     def __init__(self,
                  input_size=784,
@@ -11,16 +12,37 @@ class NeuralNetwork:
 
         self.layers = []
 
-        # Default hidden_sizes
+        # Normalize hidden_sizes
         if hidden_sizes is None:
             hidden_sizes = []
-
-        # If user passes a single int, convert to list
-        if isinstance(hidden_sizes, int):
+        elif isinstance(hidden_sizes, int):
             hidden_sizes = [hidden_sizes]
+        else:
+            hidden_sizes = list(hidden_sizes)
 
-        
-        # (Direct connection from input to output)
+        # Infer num_layers if not given
+        if num_layers is None:
+            num_layers = len(hidden_sizes)
+
+        # Make hidden_sizes compatible with num_layers
+        if num_layers == 0:
+            hidden_sizes = []
+        else:
+            if len(hidden_sizes) == 0:
+                raise ValueError("hidden_sizes must be provided when num_layers > 0")
+            elif len(hidden_sizes) == 1 and num_layers > 1:
+                hidden_sizes = hidden_sizes * num_layers
+            elif len(hidden_sizes) != num_layers:
+                raise ValueError("num_layers must match length of hidden_sizes")
+
+        self.input_size = input_size
+        self.hidden_sizes = hidden_sizes
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.activation = activation
+        self.weight_init = weight_init
+
+        # No hidden layers
         if num_layers == 0:
             self.layers.append(
                 NeuralLayer(
@@ -31,9 +53,7 @@ class NeuralNetwork:
                 )
             )
         else:
-
             # First hidden layer
-
             self.layers.append(
                 NeuralLayer(
                     input_size=input_size,
@@ -47,16 +67,14 @@ class NeuralNetwork:
             for i in range(1, num_layers):
                 self.layers.append(
                     NeuralLayer(
-                        input_size=hidden_sizes[i-1],
+                        input_size=hidden_sizes[i - 1],
                         output_size=hidden_sizes[i],
                         activation=activation,
                         weight_init=weight_init
                     )
                 )
 
-            # Output layer 
-            # last hidden to output classes
-            # activation - softmax
+            # Output layer
             self.layers.append(
                 NeuralLayer(
                     input_size=hidden_sizes[-1],
@@ -66,22 +84,17 @@ class NeuralNetwork:
                 )
             )
 
-    # FORWARD PASS
     def forward(self, X):
         out = X
         for layer in self.layers:
             out = layer.forward(out)
         return out
 
-
-    # BACKWARD PASS
-
     def backward(self, dA):
         grad = dA
         for layer in reversed(self.layers):
             grad = layer.backward(grad)
-            
-        return grad 
+        return grad
 
     def get_layers(self):
         return self.layers
